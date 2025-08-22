f2b4c44d1b47bf46aa66679cf6401d33cebbc796
amanlad97 <ladaman7@gmail.com>
2025-08-22 09:15:08 +0530

    login and signup implimented

--- apps/https-server/src/api/security.ts
+++ apps/https-server/src/api/security.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
-
+import bcrypt from "bcrypt";
 import { JWT_KEY } from "@repo/backend-common/config";
 import { CreateUserSchema, SignInSchema } from "@repo/common/types";
 import { prismaClient } from "@repo/db/prisma";
@@ -9,32 +9,78 @@ const jwtKey = JWT_KEY;
 
 export const security: Router = Router();
 
-security.post("/login", (req, res) => {
-  console.log("qwert");
-  const data = CreateUserSchema.safeParse(req.body);
-  if (!data.success) {
-    res
+security.post("/login", async (req, res) => {
+  const data = SignInSchema.safeParse(req.body);
+  if (!data.data || !data.success) {
+    return res
       .json({
         ok: false,
         message: "wrong data send ",
       })
       .status(403);
   }
+
+  const salt = await bcrypt.genSalt(5);
+  const hashedPassword = await bcrypt.hash(data.data?.password, salt);
+  const temp = await prismaClient.user
+    .findFirst({
+      where: {
+        username: data.data.username,
+        password: hashedPassword,
+      },
+    })
+    .catch((error) => {
+      return res.json({
+        message: "oops something went wrong",
+        error,
+      });
+    });
+  console.log(temp);
   const encoded = jwt.sign({ id: data.data?.username }, jwtKey);
   res.json({
     token: encoded,
   });
 });
 
-security.post("signin", (req, res) => {
-  const data = SignInSchema.safeParse(req.body);
-  if (!data.success) {
-    res
+security.post("signup", async (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.data || !data.success) {
+    return res
       .json({
         ok: false,
         message: "wrong data send ",
       })
       .status(403);
   }
-  res.json({});
+  const existCheck = prismaClient.user.findFirst({
+    where: {
+      username: data.data.username,
+    },
+  });
+
+  console.log(existCheck);
+
+  const salt = await bcrypt.genSalt(5);
+  const hashedPassword = await bcrypt.hash(data.data?.password, salt);
+  const temp = await prismaClient.user
+    .create({
+      data: {
+        username: data.data.username,
+        password: hashedPassword,
+        name: data.data.name,
+      },
+    })
+    .catch((error) => {
+      res.json({
+        message: "oops something went wrong",
+        error,
+      });
+    });
+
+  res
+    .json({
+      ok: false,
+      message: "signup was successful !!!",
+    })
+    .status(200);
 });
