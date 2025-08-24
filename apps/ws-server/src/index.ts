0795a02c798112f11e905386e7dfb952431615c7
amanlad97 <ladaman7@gmail.com>
2025-08-24 16:06:17 +0530

    web socket sorted structure ready

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,30 +1,66 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_KEY } from "@repo/backend-common/config";
 const jwtKey = JWT_KEY;
 interface User {
   userId: number;
-  rooms: String[];
+  rooms: number[];
   ws: WebSocket;
 }
-const users = [];
+interface payload {
+  type: "join" | "chat" | "leave";
+  message: string;
+  roomId: number;
+}
+const users: User[] = [];
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function connection(ws, request) {
   if (!request.url) {
     return;
   }
+  //TODO: put the token in header
   const queryParams = new URLSearchParams(request.url.split("?")[1]);
   const token = queryParams.get("token") || "";
-  const decoded = jwt.verify(token, jwtKey);
-  if (!decoded || !(decoded as JwtPayload).id) {
-    ws.close();
-    return;
+
+  try {
+    const decoded = jwt.verify(token, jwtKey);
+    if (!decoded || typeof decoded === "string") {
+      ws.close();
+      return;
+    }
+    users.push({ userId: decoded.id, ws: ws, rooms: [] });
+  } catch (error) {
+    console.log(error);
   }
+  ws.on("message", function message(message: String) {
+    console.log(users);
+    const payload: payload = JSON.parse(message.toString());
+    switch (payload.type) {
+      case "chat":
+        users.forEach((user) => {
+          if (user.rooms.includes(payload.roomId)) {
+            user.ws.send(
+              JSON.stringify({ message: payload.message, chat: payload.type })
+            );
+          }
+        });
+        break;
 
-  queryParams.get("room");
+      case "join":
+        users.find((user) => {
+          user.rooms.push(payload.roomId);
+        });
+        break;
 
-  ws.on("message", function message(message) {
-    ws.send("pong");
+      case "leave":
+        users.find((user) => {
+          user.rooms = user.rooms.filter((x) => x !== payload.roomId);
+        });
+        break;
+
+      default:
+        break;
+    }
   });
 });
