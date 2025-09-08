d43902a138b94b9f49666e1dd339e21db8449d6f
amanlad97 <ladaman7@gmail.com>
2025-09-08 18:05:54 +0530

    initial drawing logic added

--- apps/web/app/dashboard/draw.ts
+++ apps/web/app/dashboard/draw.ts
@@ -1,3 +1,45 @@
-const draw = (ctx: CanvasRenderingContext2D) => {
-  
+export const draw = (canvas: HTMLCanvasElement): void => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.style.background = "black";
+
+  const final: { x: number; y: number; width: number; height: number }[] = [];
+  const ctx = canvas.getContext("2d");
+  if (!ctx) return;
+  let isclicked = false;
+  let x = 0;
+  let y = 0;
+  const mousedown = (event: MouseEvent) => {
+    const rect = canvas.getBoundingClientRect();
+    isclicked = true;
+    x = event.clientX - rect.left;
+    y = event.clientY - rect.top;
+  };
+  const mousemove = (event: MouseEvent) => {
+    if (isclicked) {
+      const width = event.clientX - x;
+      const height = event.clientY - y;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "white";
+      ctx.strokeRect(x, y, width, height);
+      final.forEach((r) => {
+        ctx.strokeRect(r.x, r.y, r.width, r.height);
+      });
+    }
+  };
+  const mouseup = (event: MouseEvent) => {
+    if (isclicked) {
+      final.push({
+        x,
+        y,
+        width: event.clientX - x,
+        height: event.clientY - y,
+      });
+      isclicked = false;
+    }
+  };
+  canvas.addEventListener("mousedown", mousedown);
+  canvas.addEventListener("mouseup", mouseup);
+  canvas.addEventListener("mousemove", mousemove);
 };
--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 import { useEffect, useRef } from "react";
+import { draw } from "./draw";
 
 const Dashboard = () => {
   const drawing = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
     if (drawing.current) {
       const canvas = drawing.current;
-      const ctx = canvas.getContext("2d");
-      if (!ctx) return;
-      ctx.fillRect(30, 30, 30, 30);
-      ctx.stroke();
-      console.log(ctx);
+      //   canvas.width = window.innerWidth;
+      //   canvas.height = window.innerHeight;
+      draw(canvas);
     }
   }, [drawing]);
   return <canvas ref={drawing} className="w-screen h-screen"></canvas>;
