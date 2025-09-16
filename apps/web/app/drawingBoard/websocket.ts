4af696e448f68e4087da590973baedf888766b73
amanlad97 <ladaman7@gmail.com>
2025-09-16 19:17:15 +0530

    websocket added to config and bit of a cleanup

--- apps/web/app/drawingBoard/websocket.ts
+++ apps/web/app/drawingBoard/websocket.ts
@@ -1,6 +1,8 @@
+import { WEBSOCKET_URL } from "@repo/common/utils";
+
 export const connectWebSocket = (roomId: number): WebSocket => {
   const token = localStorage.getItem("token");
-  const ws = new WebSocket(`ws://localhost:8080?token=${token}`);
+  const ws = new WebSocket(`${WEBSOCKET_URL}?token=${token}`);
   ws.onopen = (event: Event) => {
     console.log("WebSocket connected:", event);
     joinRoom(ws, roomId);
