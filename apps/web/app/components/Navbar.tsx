9581edcb55a57a2c2b417eaa7e465a04cf64c842
amanlad97 <ladaman7@gmail.com>
2025-11-14 09:04:12 +0530

    navbar fixed to suite the new approach

--- apps/web/app/components/Navbar.tsx
+++ apps/web/app/components/Navbar.tsx
@@ -1,31 +1,31 @@
 "use client";
-
-import { UseUser } from "../hooks/UseUser";
-const logout = () => {
-  if (window) {
-    localStorage.removeItem("user");
-    window.location.reload();
-  }
-};
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { deleteMyCookieAction } from "../utils/utils";
 export const Navbar = () => {
-  const user = UseUser() || {};
+  const path = usePathname();
+  const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    setLoggedIn(!(path === "/signin"));
+  }, [path]);
+
   return (
     <nav
       className="fixed top-0 left-0 right-0 z-50 
         flex items-center justify-between
         h-16 px-6 
         backdrop-blur-md bg-gray-800/70
-      text-white shadow-md"
+        text-white shadow-md"
     >
-      <div className="font-semibol padding 1rem bg-gray-900/70 ">
-        Lets Draw together
+      <div className="font-semibold px-4 py-2 bg-gray-900/70">
+        Let’s Draw Together
       </div>
-      {user && (
+      {loggedIn && (
         <button
           className="font-semibold hover:text-amber-300 transition"
-          onClick={logout}
+          onClick={deleteMyCookieAction}
         >
-          logout
+          Logout
         </button>
       )}
     </nav>
